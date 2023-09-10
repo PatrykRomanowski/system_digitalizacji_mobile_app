2d42487ccbb0e7c3d4994790a8eace0c15f08ee8
PatrykRomanowski <patrykromanowski90@gmail.com>
2023-09-11 00:06:04 +0200

    add showDocuments screen + logic

--- src/documentsScreen.js
+++ src/documentsScreen.js
@@ -1,19 +1,77 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ActivityIndicator,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import { ref, get } from "firebase/database";
 
 import { firebaseRealtime } from "./firebase";
 
 import { useSelector } from "react-redux";
 
+import { useNavigation } from "@react-navigation/native";
+
 const DocumentScreen = () => {
   const actualUserId = useSelector((state) => state.userStatus.userId);
 
-  const [allCategory, setAllCategory] = useState([]);
+  const navigation = useNavigation();
+
   const [allItem, setAllItem] = useState([]);
+  const [sortAllItem, setSortAllItem] = useState([]);
+  const [indicatorIsActive, setIndicatorIsActive] = useState(false);
+  const [descriptionValue, setDescriptionValue] = useState("");
+  const [categoryValue, setCategoryValue] = useState("");
+
+  useEffect(() => {
+    if (descriptionValue || categoryValue) {
+      if (descriptionValue && categoryValue) {
+        const sortItems = allItem.filter((item) => {
+          return item.itemDescription
+            .toLowerCase()
+            .includes(descriptionValue.toLowerCase());
+        });
+        const additionalSort = sortItems.filter((item) => {
+          return item.itemCategory
+            .toLowerCase()
+            .includes(categoryValue.toLowerCase());
+        });
+        setSortAllItem(additionalSort);
+        return;
+      }
+      if (descriptionValue) {
+        const sortItems = allItem.filter((item) => {
+          return item.itemDescription
+            .toLowerCase()
+            .includes(descriptionValue.toLowerCase());
+        });
+        setSortAllItem(sortItems);
+        return;
+      }
+      if (categoryValue) {
+        const additionalSort = allItem.filter((item) => {
+          return item.itemCategory
+            .toLowerCase()
+            .includes(categoryValue.toLowerCase());
+        });
+        setSortAllItem(additionalSort);
+        return;
+      }
+
+      return;
+    } else {
+      setSortAllItem([]);
+      return;
+    }
+  }, [descriptionValue, categoryValue]);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIndicatorIsActive(true);
       try {
         const getCategoryRef = ref(
           firebaseRealtime,
@@ -39,8 +97,8 @@ const DocumentScreen = () => {
             });
           }
         }
-        setAllCategory(categories);
         setAllItem(items);
+        setIndicatorIsActive(false);
       } catch (error) {
         console.log(error);
       }
@@ -49,13 +107,45 @@ const DocumentScreen = () => {
     fetchData();
   }, []);
 
-  const getPhotos = () => {
+  const getPhotos = (props) => {
+    console.log("BX " + props.itemCategory);
+    console.log("BX " + props.itemId);
+
+    navigation.navigate("GalleryDocument", {
+      itemCategory: props.itemCategory,
+      itemId: props.itemId,
+    }); // Przechodzi do ekranu 'Home2'
+
     console.log("XD");
   };
 
   const showAllItem = allItem.map((item) => (
     <TouchableOpacity
-      onPress={getPhotos}
+      onPress={() =>
+        getPhotos({ itemCategory: item.itemCategory, itemId: item.itemId })
+      }
+      style={styles.itemContainer}
+      key={item.itemId}
+    >
+      <View>
+        <Text style={styles.textDescription}>{item.itemDescription}</Text>
+        <View>
+          <Text style={styles.textCategoryContainer}>Kategoria:</Text>
+          <Text style={styles.textCategory}>{item.itemCategory}</Text>
+        </View>
+      </View>
+      <View style={styles.sizeContainer}>
+        <Text>Rozmiar: </Text>
+        <Text style={styles.textSize}> {item.itemSize.toFixed(3)} MB</Text>
+      </View>
+    </TouchableOpacity>
+  ));
+
+  const showAllSortItem = sortAllItem.map((item) => (
+    <TouchableOpacity
+      onPress={() =>
+        getPhotos({ itemCategory: item.itemCategory, itemId: item.itemId })
+      }
       style={styles.itemContainer}
       key={item.itemId}
     >
@@ -74,10 +164,29 @@ const DocumentScreen = () => {
   ));
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Document Screen</Text>
-      {showAllItem}
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {indicatorIsActive ? (
+          <ActivityIndicator size="large" style={styles.indicator} />
+        ) : null}
+        <TextInput
+          style={styles.input}
+          placeholder="Wyszukaj po opisie"
+          value={descriptionValue}
+          onChangeText={(text) => setDescriptionValue(text)}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Wyszukaj po kategorii"
+          value={categoryValue}
+          onChangeText={(text) => setCategoryValue(text)}
+        />
+        <Text style={styles.text}>Twoje dokumenty</Text>
+
+        {descriptionValue || categoryValue ? showAllSortItem : showAllItem}
+      </View>
+    </ScrollView>
   );
 };
 
@@ -89,24 +198,46 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24,
+    marginBottom: 20,
+    marginTop: 30,
+  },
+  input: {
+    width: 300,
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    // marginBottom: 40,
+    paddingLeft: 10,
+    marginTop: 20,
   },
   textSize: {
     textAlign: "center",
     fontWeight: "bold",
     color: "red",
+    fontSize: 12,
+  },
+  indicator: {
+    position: "absolute",
+    top: 0, // Umieść indicator na górze
+    bottom: 0, // Lub użyj `right` i `left` zamiast `top` i `bottom` do pozycjonowania w poziomie
+    right: 0,
+    left: 0,
+    backgroundColor: "rgba(0, 0, 0, 0.6)", // Przezroczyste tło
+    justifyContent: "center",
+    alignItems: "center",
   },
   textDescription: {
-    fontSize: 26,
+    fontSize: 20,
   },
-  textCategory: { fontWeight: "bold" },
-  textCategoryContainer: { marginTop: 10 },
+  textCategory: { fontWeight: "bold", fontSize: 12 },
+  textCategoryContainer: { marginTop: 7, fontSize: 12 },
   itemContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     borderWidth: 1,
     borderRadius: 15,
     borderColor: "gray",
-    padding: 16,
+    padding: 12,
     marginBottom: 15,
     width: "90%",
     // elevation: 13, // Dodaj efekt cienia
---
+++ src/showDocumentGallery.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
+import Gallery from "react-native-image-gallery";
+
+import { listAll, ref, getDownloadURL } from "firebase/storage";
+import { myStorage } from "./firebase";
+
+import { useSelector, useDispatch } from "react-redux";
+
+const ShowDocumentGallery = ({ route }) => {
+  const actualUserId = useSelector((state) => state.userStatus.userId);
+
+  const [items, setItems] = useState();
+  const [imageList, setImageList] = useState([]);
+  const [indicatorIsActive, setIndicatorIsActive] = useState(false);
+
+  const getPhotos = async () => {
+    const category = route.params.itemCategory;
+    const itemId = route.params.itemId;
+    console.log("XD:" + route.params.itemCategory);
+    console.log("XD:" + route);
+
+    const actualRef = `/users/${actualUserId}/document/${category}/${itemId}`;
+    //////////////////////////////tutaj zmiany
+    console.log(actualRef);
+    const storageRef = ref(myStorage, actualRef);
+
+    listAll(storageRef)
+      .then(async (res) => {
+        setIndicatorIsActive(true);
+        const imageUrls = await Promise.all(
+          res.items.map(async (item) => {
+            try {
+              const url = await getDownloadURL(item);
+              return {
+                source: { uri: url },
+                dimensions: {
+                  width: 800,
+                  height: 600,
+                },
+              };
+            } catch (error) {
+              console.error("Błąd pobierania URL-a obrazu:", error);
+              return null; // Lub inna wartość, jeśli wystąpił błąd
+            }
+          })
+        );
+
+        console.log("imageUrls:", imageUrls);
+
+        setImageList(imageUrls);
+        setIndicatorIsActive(false);
+      })
+      .catch((err) => console.error("Błąd pobierania listy obrazów:", err));
+
+    console.log(imageList);
+  };
+
+  useEffect(() => {
+    getPhotos();
+  }, []);
+
+  useEffect(() => {
+    console.log("BB:" + imageList);
+  }, [imageList]);
+
+  return (
+    <View style={styles.container}>
+      {indicatorIsActive ? (
+        <ActivityIndicator size="large" style={styles.indicator} />
+      ) : null}
+      <Gallery
+        style={{ flex: 1 }}
+        images={imageList}
+        initialPage={0} // Opcjonalnie: określ stronę startową
+        resizeMode="stretch"
+        // onPageSelected={onPageNext}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  indicator: {
+    position: "absolute",
+    top: 0, // Umieść indicator na górze
+    bottom: 0, // Lub użyj `right` i `left` zamiast `top` i `bottom` do pozycjonowania w poziomie
+    right: 0,
+    left: 0,
+    // backgroundColor: "rgba(0, 0, 0, 0.6)", // Przezroczyste tło
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default ShowDocumentGallery;
